0bcea8d6d3ccba94a36d409d0e5c9658a9f885f9
chederavikiran <chederavi44@gmail.com>
2025-07-18 15:12:21 +0530

    Refactor JavaScript and CSS for improved functionality and maintainability

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const menuBtn = document.querySelector('.menu');
+const menu = document.querySelector('.about-div');
+const navbar = document.querySelector('.navbar');
+
+menuBtn.addEventListener("click",()=>{
+    if(menuBtn.innerText ==="menu"){
+        menu.style.opacity = '1';
+        menuBtn.innerText ="close";
+
+    }
+    else{
+          menu.style.opacity = '0';
+        menuBtn.innerText ="menu";
+    }
+})
+
+
+window .addEventListener('scroll',()=>{
+    if (window.scrollY > window.innerHeight-50){
+navbar.classList.add('navbar-active');
+
+    }
+       if (window.scrollY < window.innerHeight-50){
+navbar.classList.remove('navbar-active');
+
+    }
+
+})
+
 
 
 function firstPageAnim(){
